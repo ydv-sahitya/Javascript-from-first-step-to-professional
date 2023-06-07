c79654d142027dfb73d8a70cfdf75d38da16245c
ydv-sahitya <11sahitya@gmail.com>
2023-06-07 19:37:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+document.querySelector("header h2").append("and love")
+
+// if i want to replace totally.
+
+document.querySelector("header h2").textContent = "A game you know and like"
+
+document.getElementById("p1-symbol").textContent = "x" 
